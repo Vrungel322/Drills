219f9e0531ea171510ff5d814494fac7b85327ed
Nikita Romanenko <nikita241296@gmail.com>
2017-05-01 15:30:41 +0300

    Done btn add

--- app/src/main/java/com/udtech/drills/feature/holoshenie/fragments/HoloshenieFragment.java
+++ app/src/main/java/com/udtech/drills/feature/holoshenie/fragments/HoloshenieFragment.java
@@ -1,13 +1,17 @@
 package com.udtech.drills.feature.holoshenie.fragments;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 import butterknife.BindView;
+import butterknife.OnClick;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.udtech.drills.R;
 import com.udtech.drills.base.BaseFragment;
 import com.udtech.drills.data.remote.fetch_user_data.Practic;
+import com.udtech.drills.feature.content.fragments.ContentFragment;
 import com.udtech.drills.feature.holoshenie.adapters.PracticeAdapter;
 import com.udtech.drills.feature.holoshenie.presenters.HoloshenieFragmentPresenter;
 import com.udtech.drills.feature.holoshenie.views.IHoloshenieFragmentView;
@@ -18,6 +22,7 @@ public class HoloshenieFragment extends BaseFragment implements IHoloshenieFragm
   @InjectPresenter HoloshenieFragmentPresenter mHoloshenieFragmentPresenter;
 
   @BindView(R.id.rvPractice) RecyclerView mRecyclerViewPractice;
+  @BindView(R.id.tvDone) TextView mTextViewDone;
 
   private PracticeAdapter mPracticeAdapter;
 
@@ -36,12 +41,16 @@ public class HoloshenieFragment extends BaseFragment implements IHoloshenieFragm
     mPracticeAdapter = new PracticeAdapter();
     mRecyclerViewPractice.setLayoutManager(new LinearLayoutManager(getContext()));
     mRecyclerViewPractice.setAdapter(mPracticeAdapter);
-
   }
 
   @Override public void fillInRecyclerView(List<Practic> practic) {
 
     Timber.e("" + practic.size());
     mPracticeAdapter.addListPractic(practic);
+  }
+
+  @OnClick(R.id.tvDone) public void tvDoneClick() {
+    mNavigator.replaceFragment((AppCompatActivity) getActivity(), R.id.contentContainer,
+        ContentFragment.newInstance());
   }
 }
