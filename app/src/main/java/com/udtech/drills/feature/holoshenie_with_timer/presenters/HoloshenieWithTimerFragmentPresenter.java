89217c30edb967a66dd2f4375d09e00bd7397612
Vrungel <nikita241296@gmail.com>
2017-05-12 12:51:25 +0300

    HoloshenieWithTimerFragment add

---
+++ app/src/main/java/com/udtech/drills/feature/holoshenie_with_timer/presenters/HoloshenieWithTimerFragmentPresenter.java
@@ -0,0 +1,8 @@
+package com.udtech.drills.feature.holoshenie_with_timer.presenters;
+
+/**
+ * Created by Vrungel on 12.05.2017.
+ */
+
+public class HoloshenieWithTimerFragmentPresenter {
+}
---
+++ app/src/main/java/com/udtech/drills/feature/holoshenie_with_timer/views/IHoloshenieWithTimerFragmentView.java
@@ -0,0 +1,8 @@
+package com.udtech.drills.feature.holoshenie_with_timer.views;
+
+/**
+ * Created by Vrungel on 12.05.2017.
+ */
+
+public interface IHoloshenieWithTimerFragmentView {
+}
