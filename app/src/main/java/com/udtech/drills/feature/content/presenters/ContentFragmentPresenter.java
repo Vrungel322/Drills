2eb89cad25aa3d9b7768b1b847566a842047c5bb
Dimuch_ <antibog8@gmail.com>
2017-05-23 02:05:35 +0300

    Add grouping days per four weeks (return status of days per 4 weeks and return total time per each weeks).

--- app/src/main/java/com/udtech/drills/feature/content/presenters/ContentFragmentPresenter.java
+++ app/src/main/java/com/udtech/drills/feature/content/presenters/ContentFragmentPresenter.java
@@ -4,12 +4,18 @@ import com.arellomobile.mvp.InjectViewState;
 import com.udtech.drills.App;
 import com.udtech.drills.base.BasePresenter;
 import com.udtech.drills.data.DataManager;
+import com.udtech.drills.data.local.mappers.show_history.GroupingDaysIntoWeeks;
 import com.udtech.drills.data.remote.login.User;
 import com.udtech.drills.feature.content.views.IContentFragmentView;
+import com.udtech.drills.utils.Converters;
 import com.udtech.drills.utils.RxBus;
 import com.udtech.drills.utils.RxBusHelper;
 import com.udtech.drills.utils.ThreadSchedulers;
+
+import java.util.List;
+
 import javax.inject.Inject;
+
 import rx.Observable;
 import rx.Subscription;
 import timber.log.Timber;
@@ -38,9 +44,35 @@ import timber.log.Timber;
         .compose(ThreadSchedulers.applySchedulers())
         .concatMap(synchronizeData -> {
           synchronizeData();
+          getDaysForCalendar();
           return Observable.just("");
         })
         .subscribe(o -> {
+        });
+    addToUnsubscription(subscription);
+  }
+
+  private void getDaysForCalendar() {
+    Subscription subscription =
+        mDataManager.getHistoryFromDb().compose(ThreadSchedulers.applySchedulers()).
+            concatMap(historyForSendList -> {
+
+              List<Integer> listStatusOfDay =
+                  new GroupingDaysIntoWeeks().getListStatusOfDay(historyForSendList);
+
+              for (Integer status : listStatusOfDay) {
+                Timber.e(status.toString());
+              }
+
+              List<Integer> listTotalTimeOfTheWeek =
+                  new GroupingDaysIntoWeeks().getListTotalTimeOfTheWeek(historyForSendList);
+
+              for (Integer totalTimeOfTheWeek : listTotalTimeOfTheWeek) {
+                Timber.e(Converters.timeFromSeconds(String.valueOf(totalTimeOfTheWeek)));
+              }
+
+              return Observable.just("");
+            }).subscribe(o -> {
         });
     addToUnsubscription(subscription);
   }
